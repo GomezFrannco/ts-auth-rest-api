5c997ae284a2180f08ab91378cb0db2047507505
GomezFrannco <frangomez.baires@gmail.com>
2022-07-29 19:09:16 -0300

    [Add] Zod user schema has been created

---
+++ src/schemas/user.schemas.ts
@@ -0,0 +1,24 @@
+import { object, string } from "zod";
+
+export const createUserSchema = object({
+  body: object({
+    firstName: string({
+      required_error: "First name is required",
+    }),
+    lastName: string({
+      required_error: "Last name is required",
+    }),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password too short - min 6 characters"),
+    passwordConfirmation: string({
+      required_error: "Password confirmation is required",
+    }),
+    email: string({
+      required_error: "Email is required",
+    }).email("Please type a valid email address"),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords must be equals",
+    path: ["passwordConfirmation"],
+  }),
+});
