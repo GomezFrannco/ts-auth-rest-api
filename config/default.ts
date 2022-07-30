a58a9f216bd6d8c048fd89d707161c324a61ce09
GomezFrannco <frangomez.baires@gmail.com>
2022-07-30 18:06:21 -0300

    [Add] smtp variables has been added

--- config/default.ts
+++ config/default.ts
@@ -3,4 +3,11 @@ import "../src/utils/dotenv.utils";
 export default {
   port: process.env.PORT,
   mongo_uri: process.env.MONGO_URI,
+  smtp: {
+    user:process.env.SMTP_USER,
+    pass:process.env.SMTP_PASS,
+    host:process.env.SMTP_HOST,
+    port:process.env.SMTP_PORT,
+    secure: false
+  }
 };
