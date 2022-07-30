5e9ea717eb6c367eeb16c10c25449931d8c62c16
GomezFrannco <frangomez.baires@gmail.com>
2022-07-30 18:07:25 -0300

    [Add] smtp type has been created

--- src/types/api.types.ts
+++ src/types/api.types.ts
@@ -2,3 +2,11 @@ import { TypeOf } from "zod";
 import { createUserSchema } from "../schemas/user.schemas";
 
 export type CreateUserInput = TypeOf<typeof createUserSchema>["body"];
+
+export type smptType = {
+  user: string;
+  pass: string;
+  host: string;
+  port: number;
+  secure: boolean;
+};
