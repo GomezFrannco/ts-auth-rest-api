efbd0cd81d8fe1b8373530a5cbe5658cffd06480
GomezFrannco <frangomez.baires@gmail.com>
2022-07-30 18:08:53 -0300

    [Add] Create user controller has been created

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { createUser } from "../services/user.services";
+import { CreateUserInput } from "../types/api.types";
+import sendEmail from "../utils/mailer.utils";
+
+export async function createUserHandler(req: Request<{}, {}, CreateUserInput>, res: Response) {
+  const body = req.body;
+  try {
+    const user = await createUser(body);
+    await sendEmail({
+      from: 'test@example.com',
+      to: user.email,
+      subject: 'Verify your account',
+      text: `Here is your verification code: ${user.verificationCode}\n ID: ${user._id}` 
+    });
+    return res.send("User created successfully!");
+  } catch (e: any) {
+    if (e.code == 11000) {
+      return res.status(409).send("Account already exists!");
+    }
+    return res.status(500).send(e)
+  }
+}
