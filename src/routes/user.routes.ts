6d92122b3b45216ca0b30c1f7a208a517c7e4cde
GomezFrannco <frangomez.baires@gmail.com>
2022-07-30 18:09:57 -0300

    [Updt] Create user endpoint has been added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { createUserHandler } from "../controllers/user.controllers";
+import validateResource from "../middlewares/validate.middlewares";
+import { createUserSchema } from "../schemas/user.schemas";
 
 const router = Router();
 
-
+router.post("/api/users", validateResource(createUserSchema), createUserHandler);
 
 export default router;
