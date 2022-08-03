e493b14d97828f2dec1046f8da32bf5e5faa9256
GomezFrannco <frangomez.baires@gmail.com>
2022-08-02 23:11:47 -0300

    [Add] forgot password endpoint has been created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { createUserHandler, verifyUserHandler } from "../controllers/user.controllers";
+import { createUserHandler, forgotPasswordHandler, verifyUserHandler } from "../controllers/user.controllers";
 import validateResource from "../middlewares/validate.middlewares";
-import { createUserSchema, verifyUserSchema } from "../schemas/user.schemas";
+import { createUserSchema, forgotPasswordSchema, verifyUserSchema } from "../schemas/user.schemas";
 
 const router = Router();
 
 router.post("/api/users", validateResource(createUserSchema), createUserHandler);
 
 router.post("/api/users/verify/:id/:verificationCode", validateResource(verifyUserSchema), verifyUserHandler);
+
+router.post("/api/users/forgotpassword",  validateResource(forgotPasswordSchema), forgotPasswordHandler);
 
 export default router;
