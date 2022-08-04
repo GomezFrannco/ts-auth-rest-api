ff9bd5765e2a0b644c72aa3c24a30e4b35e1ac09
GomezFrannco <frangomez.baires@gmail.com>
2022-08-03 23:14:29 -0300

    [Add] JWT tokens key has been added

--- config/default.ts
+++ config/default.ts
@@ -3,6 +3,10 @@ import "../src/utils/dotenv.utils";
 export default {
   port: process.env.PORT,
   mongo_uri: process.env.MONGO_URI,
+  accessTokenPrivateKey: process.env.ACCESS_PRIVATE_KEY,
+  accessTokenPublicKey: process.env.ACCESS_PUBLIC_KEY,
+  refreshTokenPrivateKey: process.env.REFRESH_PRIVATE_KEY,
+  refreshTokenPublicKey: process.env.REFRESH_PUBLIC_KEY,
   smtp: {
     user:process.env.SMTP_USER,
     pass:process.env.SMTP_PASS,
