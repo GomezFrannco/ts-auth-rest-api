9c2876b55a52ad55a71d617c1b12fa5d5cb7f892
GomezFrannco <frangomez.baires@gmail.com>
2022-08-04 01:09:17 -0300

    [Add] refresh token handler has been created

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
-import { signAccessToken, signRefreshToken } from "../services/auth.services";
-import { findUserByEmail } from "../services/user.services";
+import { get } from "lodash";
+import { findSessionById, signAccessToken, signRefreshToken } from "../services/auth.services";
+import { findUserByEmail, findUserById } from "../services/user.services";
 import { CreateSessionInput } from "../types/api.types";
+import { verifyJwt } from "../utils/jwt.utils";
 
 export async function createSessionHandler(req: Request<{}, {}, CreateSessionInput>, res: Response) {
   const { email, password } = req.body;
@@ -22,4 +24,27 @@ export async function createSessionHandler(req: Request<{}, {}, CreateSessionInp
     accessToken,
     refreshToken,
   })
+}
+
+export async function refreshAccessTokenHandler(req: Request, res: Response) {
+  const refreshToken = get(req, 'header.x-refresh');
+  const decoded = verifyJwt<{ session: string }>(refreshToken, 'refreshTokenPublicKey')
+  if(!decoded) {
+    res.status(401).send("Could not refresh token")
+  } else {
+    const session = await findSessionById(decoded.session);
+    if(!session || !session.valid) {
+      res.status(401).send("Could not refresh token")
+    }
+    if(session) {
+      const user = await findUserById(String(session.user));
+      if(!user) {
+        res.status(401).send("Could not refresh token")
+      }
+      if (user) {
+        const accessToken = signAccessToken(user);
+        res.send({ accessToken });
+      }
+    }
+  }
 }
