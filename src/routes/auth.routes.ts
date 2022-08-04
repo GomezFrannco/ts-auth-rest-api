85782fda06c490c86f0d1574be1d98e52289909a
GomezFrannco <frangomez.baires@gmail.com>
2022-08-03 23:34:01 -0300

    [Add] create session endpoint has been created

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { createSessionHandler } from "../controllers/auth.controllers";
+import validateResource from "../middlewares/validate.middlewares";
+import { createSessionSchema } from "../schemas/auth.schemas";
 
 const router = Router();
 
-
+router.post("/api/sessions", validateResource(createSessionSchema), createSessionHandler);
 
 export default router;
