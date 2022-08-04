634c0862ecb95ad467e5e44d02c99d33c69066fa
GomezFrannco <frangomez.baires@gmail.com>
2022-08-04 01:09:54 -0300

    [Updt] refresh token endpoint has been created

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createSessionHandler } from "../controllers/auth.controllers";
+import { createSessionHandler, refreshAccessTokenHandler } from "../controllers/auth.controllers";
 import validateResource from "../middlewares/validate.middlewares";
 import { createSessionSchema } from "../schemas/auth.schemas";
 
 const router = Router();
 
 router.post("/api/sessions", validateResource(createSessionSchema), createSessionHandler);
+
+router.post("/api/sessions/refresh", refreshAccessTokenHandler);
 
 export default router;
