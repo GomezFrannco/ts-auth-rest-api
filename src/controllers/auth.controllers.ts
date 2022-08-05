5d9c21f37060f3cf3c394faa020848b8d0148243
GomezFrannco <frangomez.baires@gmail.com>
2022-08-05 18:14:06 -0300

    [Updt] handlers added into controllers class

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,50 +1,59 @@
 import { Request, Response } from "express";
 import { get } from "lodash";
-import { findSessionById, signAccessToken, signRefreshToken } from "../services/auth.services";
-import { findUserByEmail, findUserById } from "../services/user.services";
+import AuthServices from "../services/auth.services";
+import UserServices from "../services/user.services";
 import { CreateSessionInput } from "../types/api.types";
 import { verifyJwt } from "../utils/jwt.utils";
 
-export async function createSessionHandler(req: Request<{}, {}, CreateSessionInput>, res: Response) {
-  const { email, password } = req.body;
-  const user = await findUserByEmail(email);
-  if(!user) {
-    return res.status(404).send("Invalid email or password");
-  }
-  if(!user.verified) {
-    return res.status(401).send("Please verify your email");
-  }
-  const isValid = await user.validatePassword(password);
-  if(!isValid) {
-    return res.status(401).send("Invalid email or password")
-  }
-  const accessToken = signAccessToken(user);
-  const refreshToken = await signRefreshToken({ userId: user._id });
-  return res.send({
-    accessToken,
-    refreshToken,
-  })
-}
-
-export async function refreshAccessTokenHandler(req: Request, res: Response) {
-  const refreshToken = get(req, 'header.x-refresh');
-  const decoded = verifyJwt<{ session: string }>(refreshToken, 'refreshTokenPublicKey')
-  if(!decoded) {
-    res.status(401).send("Could not refresh token")
-  } else {
-    const session = await findSessionById(decoded.session);
-    if(!session || !session.valid) {
-      res.status(401).send("Could not refresh token")
-    }
-    if(session) {
-      const user = await findUserById(String(session.user));
+export default class PostAuthHandlers {
+  static async createSessionHandler (req: Request<{}, {}, CreateSessionInput>, res: Response) {
+    const { email, password } = req.body;
+    try {
+      const user = await UserServices.findUserByEmail(email);
       if(!user) {
+        return res.status(404).send("Invalid email or password");
+      }
+      if(!user.verified) {
+        return res.status(401).send("Please verify your email");
+      }
+      const isValid = await user.validatePassword(password);
+      if(!isValid) {
+        return res.status(401).send("Invalid email or password")
+      }
+      const accessToken = AuthServices.signAccessToken(user);
+      const refreshToken = await AuthServices.signRefreshToken({ userId: user._id });
+      return res.send({
+        accessToken,
+        refreshToken,
+      });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  }
+  static async refreshAccessTokenHandler(req: Request, res: Response) {
+    try {
+      const refreshToken = get(req, 'header.x-refresh');
+      const decoded = verifyJwt<{ session: string }>(refreshToken, 'refreshTokenPublicKey')
+      if(!decoded) {
         res.status(401).send("Could not refresh token")
+      } else {
+        const session = await AuthServices.findSessionById(decoded.session);
+        if(!session || !session.valid) {
+          res.status(401).send("Could not refresh token")
+        }
+        if(session) {
+          const user = await UserServices.findUserById(String(session.user));
+          if(!user) {
+            res.status(401).send("Could not refresh token")
+          }
+          if (user) {
+            const accessToken = AuthServices.signAccessToken(user);
+            res.send({ accessToken });
+          }
+        }
       }
-      if (user) {
-        const accessToken = signAccessToken(user);
-        res.send({ accessToken });
-      }
+    } catch (e) {
+      res.status(500).send(e)
     }
   }
 }
