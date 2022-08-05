8fc8379a24ee2a40656ee62a71e3f767ae1956b6
GomezFrannco <frangomez.baires@gmail.com>
2022-08-05 18:17:21 -0300

    [Updt] Handlers has been updated

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createSessionHandler, refreshAccessTokenHandler } from "../controllers/auth.controllers";
+import PostAuthHandlers from "../controllers/auth.controllers";
 import validateResource from "../middlewares/validate.middlewares";
 import { createSessionSchema } from "../schemas/auth.schemas";
 
 const router = Router();
 
-router.post("/api/sessions", validateResource(createSessionSchema), createSessionHandler);
+router.post("/api/sessions", validateResource(createSessionSchema), PostAuthHandlers.createSessionHandler);
 
-router.post("/api/sessions/refresh", refreshAccessTokenHandler);
+router.post("/api/sessions/refresh", PostAuthHandlers.refreshAccessTokenHandler);
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
-import { createUserHandler, forgotPasswordHandler, getCurrentUserHandler, resetPasswordHandler, verifyUserHandler } from "../controllers/user.controllers";
+import { PostUsersHandlers, GetUsersHandlers} from "../controllers/user.controllers";
 import requireUser from "../middlewares/require.middlewares";
 import validateResource from "../middlewares/validate.middlewares";
 import { createUserSchema, forgotPasswordSchema, resetPasswordSchema, verifyUserSchema } from "../schemas/user.schemas";
 
 const router = Router();
 
-router.post("/api/users", validateResource(createUserSchema), createUserHandler);
+router.post("/api/users", validateResource(createUserSchema), PostUsersHandlers.createUserHandler);
 
-router.post("/api/users/verify/:id/:verificationCode", validateResource(verifyUserSchema), verifyUserHandler);
+router.post("/api/users/verify/:id/:verificationCode", validateResource(verifyUserSchema), PostUsersHandlers.verifyUserHandler);
 
-router.post("/api/users/forgotpassword",  validateResource(forgotPasswordSchema), forgotPasswordHandler);
+router.post("/api/users/forgotpassword",  validateResource(forgotPasswordSchema), PostUsersHandlers.forgotPasswordHandler);
 
-router.post("/api/users/resetpassword/:id/:passwordResetCode", validateResource(resetPasswordSchema), resetPasswordHandler);
+router.post("/api/users/resetpassword/:id/:passwordResetCode", validateResource(resetPasswordSchema), PostUsersHandlers.resetPasswordHandler);
 
-router.get("/api/users/me", requireUser ,getCurrentUserHandler);
+router.get("/api/users/me", requireUser, GetUsersHandlers.getCurrentUserHandler);
 
 export default router;
